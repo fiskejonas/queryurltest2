bc7ed51b717ada14bae3a83b0118d6d147f00e76
Jonas Krogh Fischer <jonasfiskekrogh@hotmail.com>
2023-04-20 18:43:41 +0200

    hej

--- src/scripts/frames.js
+++ src/scripts/frames.js
@@ -1,53 +1,71 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const url = "https://frames-5130.restdb.io/rest/frames";
-  const options = {
-    headers: {
-      "x-apikey": "644143ee39cf552ef728c336",
-    },
-  };
+const url = "https://frames-5130.restdb.io/rest/frames";
+const options = {
+  headers: {
+    "x-apikey": "644143ee39cf552ef728c336",
+  },
+};
 
-  let filter = "alle";
-  let personer;
-  const header = document.querySelector("h1");
-  const filterKnapper = document.querySelectorAll("nav button");
-  filterKnapper.forEach((knap) =>
-    knap.addEventListener("click", filtrerPersoner)
-  );
+let filter = "alle";
+let personer;
+const header = document.querySelector("h1");
+const filterKnapper = document.querySelectorAll("nav button");
+filterKnapper.forEach((knap) =>
+  knap.addEventListener("click", filtrerPersoner)
+);
 
-  function filtrerPersoner() {
-    filter = this.dataset.aco;
+function filtrerPersoner() {
+  filter = this.dataset.aco;
 
-    visPersoner();
+  visPersoner();
 
-    header.textContent = this.textContent;
-    document.querySelector(".valgt").classList.remove("valgt");
-    this.classList.add("valgt");
-  }
+  header.textContent = this.textContent;
+  document.querySelector(".valgt").classList.remove("valgt");
+  this.classList.add("valgt");
+}
 
-  async function hentData() {
-    const JSONDATA = await fetch(url, options);
-    personer = await JSONDATA.json();
-    console.log(personer);
+async function hentData() {
+  const JSONDATA = await fetch(url, options);
+  personer = await JSONDATA.json();
 
-    visPersoner();
-  }
+  visPersoner();
+}
 
-  function visPersoner() {
-    const section = document.querySelector("section");
-    const template = document.querySelector("template").content;
-    section.textContent = "";
-    personer.forEach((person) => {
-      if (filter == person.aco || filter == "alle") {
-        const klon = template.cloneNode(true);
-        klon.querySelector(".template_navn").textContent = person.title;
-        klon.querySelector(".template_beskrivelse").textContent =
-          person.description;
-        klon.querySelector(".template_frame").src =
-          "https://wmcontent.dk/_HighImpact/" + person.link;
-        section.appendChild(klon);
-      }
-    });
-  }
+let selectedCards = new Set();
 
-  hentData();
-});
+function visPersoner() {
+  const section = document.querySelector("section");
+  const template = document.querySelector("template").content;
+  section.textContent = "";
+  personer.forEach((person) => {
+    if (filter == person.aco || filter == "alle") {
+      const klon = template.cloneNode(true);
+      klon.querySelector(".template_navn").textContent = person.title;
+      klon.querySelector(".template_frame").src =
+        "https://wmcontent.dk/_HighImpact/" + person.link;
+      klon.querySelector(".template_beskrivelse").textContent =
+        person.description;
+
+      klon.querySelector(".template_article").addEventListener("click", (e) => {
+        e.currentTarget.classList.toggle("selected");
+        if (selectedCards.has(person)) {
+          selectedCards.delete(person);
+        } else {
+          selectedCards.add(person);
+        }
+      });
+
+      section.appendChild(klon);
+    }
+  });
+}
+
+function navigateToSelectedCards() {
+  const selectedIds = Array.from(selectedCards, (card) => card._id).join(",");
+  window.location.href = `selected-cards.html?ids=${selectedIds}`;
+}
+
+document
+  .getElementById("view-selected-cards")
+  .addEventListener("click", navigateToSelectedCards);
+
+hentData();
--- src/scripts/selected-cards.js
+++ src/scripts/selected-cards.js
@@ -1,15 +1,46 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const urlParams = new URLSearchParams(window.location.search);
-  const cardIds = urlParams.get("cardIds")
-    ? urlParams.get("cardIds").split(",")
-    : [];
+// selected-cards.js
 
-  const cardsContainer = document.getElementById("cardsContainer");
+const url = "https://frames-5130.restdb.io/rest/frames";
+const options = {
+  headers: {
+    "x-apikey": "644143ee39cf552ef728c336",
+  },
+};
 
-  cardIds.forEach((cardId) => {
-    const card = document.createElement("div");
-    card.classList.add("card", "selected");
-    card.textContent = `Card: ${cardId}`;
-    cardsContainer.appendChild(card);
+function getSelectedCardIds() {
+  const queryString = new URLSearchParams(window.location.search);
+  const idsString = queryString.get("ids");
+  return idsString ? idsString.split(",") : [];
+}
+
+async function displaySelectedCards() {
+  const selectedCardIds = getSelectedCardIds();
+  if (selectedCardIds.length === 0) return;
+
+  const JSONDATA = await fetch(url, options);
+  const allCards = await JSONDATA.json();
+  const selectedCards = allCards.filter((card) =>
+    selectedCardIds.includes(card._id)
+  );
+
+  // Use visPersoner function to display selected cards
+  // Make sure to pass `selectedCards` instead of `personer`
+  visPersoner(selectedCards);
+}
+
+function visPersoner(cardsToDisplay) {
+  const section = document.querySelector("section");
+  const template = document.querySelector("template").content;
+  section.textContent = "";
+  cardsToDisplay.forEach((person) => {
+    const klon = template.cloneNode(true);
+    klon.querySelector(".template_navn").textContent = person.title;
+    klon.querySelector(".template_frame").src =
+      "https://wmcontent.dk/_HighImpact/" + person.link;
+    klon.querySelector(".template_beskrivelse").textContent =
+      person.description;
+    section.appendChild(klon);
   });
-});
+}
+
+displaySelectedCards();
